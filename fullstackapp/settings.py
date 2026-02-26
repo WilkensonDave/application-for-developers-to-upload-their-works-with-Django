270de38a5e879c56a57446d76b3e6dca90cb5116
Devariste <devaristewilkenson@gmail.com>
2026-02-26 17:29:46 -0400

    Update settings to remove secret credentials

--- fullstackapp/settings.py
+++ fullstackapp/settings.py
@@ -185,8 +185,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "wilkensondevariste@gmail.com"
-EMAIL_HOST_PASSWORD = "drak cmjj yqvz cpyh"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
